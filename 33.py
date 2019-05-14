bca8229a0ec3c22e2bf0e2f521e07230b66a243a
Beenczy <36893426+Beenczy@users.noreply.github.com>
2019-05-14 12:06:53 +0200

    Update 33.py

--- 33.py
+++ 33.py
@@ -1,2 +1,9 @@
-
-dsasd
+a=int(input('N='))
+l={'','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+c=a-1
+b=1
+for i in range (a+1):
+    a-=1
+    print(' '+(a+1),'#'*i)
+for i in range(c,-1,-1):
+    print(' '+b,'#'*i)
